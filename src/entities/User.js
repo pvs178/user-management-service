7af7d285d981c5488662fc47549ab3df3673eddb
pvs <pvs>
2025-11-13 12:42:58 +0300

    fix validation

--- src/entities/User.js
+++ src/entities/User.js
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm'
-import { IsEmail, IsNotEmpty, IsEnum, IsBoolean, IsDateString, MinLength, MaxLength } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsEnum, IsBoolean, IsDateString, MinLength, MaxDate } from 'class-validator'
 
 export const UserRole = {
   ADMIN: 'admin',
@@ -14,25 +14,23 @@ export class User {
   @Column({ type: 'varchar', length: 255 })
   @IsNotEmpty()
   @MinLength(2)
-  @MaxLength(255)
   fullName
 
   @Column({ type: 'date' })
   @IsNotEmpty()
   @IsDateString()
+  @MaxDate(new Date())
   dateOfBirth
 
   @Column({ type: 'varchar', length: 255, unique: true })
   @Index('IDX_user_email')
   @IsNotEmpty()
   @IsEmail()
-  @MaxLength(255)
   email
 
   @Column({ type: 'varchar', length: 255 })
   @IsNotEmpty()
   @MinLength(6)
-  @MaxLength(255)
   password
 
   @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
